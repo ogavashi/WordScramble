3227467739b037bf44ad319113cd18ffb4a631d3
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-08-01 18:28:28 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Oleg Gavashi on 01.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
